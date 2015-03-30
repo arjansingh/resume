27ea0b7ecf0a25787a8c5e1d093bf513c60ca62c
arjansingh <arjan.singh@gmail.com>
2015-03-29 22:10:11 -0700

    Initial resume template.

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,9 +1,16 @@
 import Ember from 'ember';
-
-var data = {};
+import contactInfo from 'arjans-resume/data/contact';
+import education from 'arjans-resume/data/education';
+import experience from 'arjans-resume/data/jobs';
+import skills from 'arjans-resume/data/skills';
 
 export default Ember.Route.extend({
   model: function () {
-    return data;
+    return {
+      'contact': contactInfo,
+      'education': education,
+      'experience': experience,
+      'skills': skills
+    };
   }
 });
